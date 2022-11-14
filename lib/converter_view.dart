b5ac2cb10e7a89ca1480e074956b895c1e933d8f
exxsid <cortezleoanthony5@gmail.com>
2022-11-14 21:49:14 +0800

    added the dropdown button for from value

--- lib/converter_view.dart
+++ lib/converter_view.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+const List<String> _lengthUnits = [
+  'centimeter',
+  'meter',
+  'kilometer',
+];
+
 class Converter extends StatefulWidget {
   const Converter({super.key});
 
@@ -8,8 +14,49 @@ class Converter extends StatefulWidget {
 }
 
 class _ConverterState extends State<Converter> {
+  String _fromDropdownValue = _lengthUnits.first;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "CONVERTER",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  letterSpacing: 5,
+                ),
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+
+              // dropdown for from value
+              DropdownButton(
+                value: _fromDropdownValue,
+                icon: Icon(Icons.arrow_downward),
+                items:
+                    _lengthUnits.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  // This is called when the user selects an item.
+                  setState(() {
+                    _fromDropdownValue = value!;
+                  });
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
